9e04efb8031a058e57d5da16b75b5880238ea95d
John Trecker <jttrecker@gmail.com>
2018-07-24 11:18:48 -0700

    cleanup

--- callisto-fse/api.py
+++ callisto-fse/api.py
@@ -1,5 +1,4 @@
 from callisto_core.notification.api import CallistoCoreNotificationApi as CallistoAPI
-import gnupg
 """
 This is a program that uses the callisto notification API to package an
 encrypted GPG file and saves it to the disk for testing encryption in a
@@ -23,20 +22,8 @@ class FSENotificationAPI(CallistoAPI):
         with open(inputFile, 'r') as report:
             return report.read()
 
-    def encrypt_file(self, file_data, public_key):
-        gpg = gnupg.GPG()
-        data = 'this is only a test'
-        imported_keys = gpg.import_keys(public_key)
-        return gpg.encrypt(
-            data,
-            imported_keys.fingerprints[0],
-            armor=True,
-            always_trust=True,
-        ).data
-
     def create_encrypted_report(self, inputFile, outputFile):
         publicKey = self.getPublicKey()
         inputData = self.getReport(inputFile)
-        encryptedData = self.encrypt_file(inputData, publicKey)
-        print ('[api.py] encryptedData', str(encryptedData))
+        encryptedData = self._encrypt_file(inputData, publicKey)
         self.writeOutputFile(encryptedData, outputFile)
